dfa52b5fa49ee0d0443564a1eca085afb93744e4
Camilo Victoria <cvictoria@vovsolutions.com>
2023-02-20 14:53:55 -0500

    pre-commit config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
