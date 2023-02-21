3ea6908ef9fcd35aa7d8524bc67691dbac72b618
Camilo Victoria <cvictoria@vovsolutions.com>
2023-02-20 20:11:28 -0500

    test: add the git command to add files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
 import Image from 'next/image';
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { Inter } from '@next/font/google';
+import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-
   return (
-              <main className={styles.main}>
-                <div className={styles.description}>
-                  <p>
-                    Get started by editing&nbsp;
-                    <code className={styles.code}>src/app/page.tsx</code>
-                  </p>
-                  <div>
-                    <a
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+        <div>
+          <a
             href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
             target='_blank'
             rel='noopener noreferrer'
